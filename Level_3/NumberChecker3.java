94f868f990bc579f97706bb43b7e58a542b46d5a
vermamaan55@gmail.com <vermamaan55@gmail.com>
2026-02-05 15:33:22 +0530

    Added NumberChecker3.java, Solved using method.

---
+++ Level_3/NumberChecker3.java
@@ -0,0 +1,73 @@
+package Java.Level_3;
+import java.util.*;
+class NumberChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int count = countDigits(num);
+        int[] digits = storeDigits(num);
+        System.out.println("Count of digits: " + count);
+        System.out.print("Digits array: ");
+        for (int d : digits) System.out.print(d + " ");
+        System.out.println();
+        int[] reversed = reverseArray(digits);
+        System.out.print("Reversed digits: ");
+        for (int d : reversed) System.out.print(d + " ");
+        System.out.println();
+        System.out.println("Arrays Equal: " + compareArrays(digits, reversed));
+        System.out.println("Palindrome Number: " + isPalindrome(digits));
+        System.out.println("Duck Number: " + isDuckNumber(digits));
+        sc.close();
+    }
+
+    public static int countDigits(int n) {
+        n = Math.abs(n);
+        if (n == 0) return 1;
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+
+    public static int[] storeDigits(int n) {
+        n = Math.abs(n);
+        int len = countDigits(n);
+        int[] digits = new int[len];
+        for (int i = len - 1; i >= 0; i--) {
+            digits[i] = n % 10;
+            n /= 10;
+        }
+        return digits;
+    }
+
+    public static int[] reverseArray(int[] arr) {
+        int[] rev = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            rev[i] = arr[arr.length - 1 - i];
+        }
+        return rev;
+    }
+
+    public static boolean compareArrays(int[] a, int[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(int[] digits) {
+        int[] rev = reverseArray(digits);
+        return compareArrays(digits, rev);
+    }
+
+    public static boolean isDuckNumber(int[] digits) {
+        for (int d : digits) {
+            if (d == 0) return true;
+        }
+        return false;
+    }
+}
