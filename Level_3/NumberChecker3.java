6cc5813b1764811fdc18dd7152de3cdced746812
vermamaan55@gmail.com <vermamaan55@gmail.com>
2026-02-05 15:38:34 +0530

    Updated NumberChecker3.javA

--- Level_3/NumberChecker3.java
+++ Level_3/NumberChecker3.java
@@ -1,6 +1,6 @@
 package Java.Level_3;
 import java.util.*;
-class NumberChecker {
+class NumberChecker3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
