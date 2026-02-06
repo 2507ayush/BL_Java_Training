a7e26366db9cd18c40b2377213d0ab02ba5b99b9
vermamaan55@gmail.com <vermamaan55@gmail.com>
2026-02-06 10:58:58 +0530

    Added StudentMarks.java, Solved by using Array and it's properties.

---
+++ Level_2/StudentMarks.java
@@ -0,0 +1,66 @@
+package Java.Level_2;
+import java.util.*;
+
+public class StudentMarks {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[] physics = new int[n];
+        int[] chemistry = new int[n];
+        int[] maths = new int[n];
+        float[] percentage = new float[n];
+        char[] grade = new char[n];
+
+        // Input marks
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter marks for Student " + (i + 1));
+
+            System.out.print("Physics: ");
+            physics[i] = sc.nextInt();
+            if (physics[i] < 0) { i--; continue; }
+
+            System.out.print("Chemistry: ");
+            chemistry[i] = sc.nextInt();
+            if (chemistry[i] < 0) { i--; continue; }
+
+            System.out.print("Maths: ");
+            maths[i] = sc.nextInt();
+            if (maths[i] < 0) { i--; }
+        }
+
+        // Calculate percentage and grade
+        for (int i = 0; i < n; i++) {
+            percentage[i] = (physics[i] + chemistry[i] + maths[i]) / 3.0f;
+
+            if (percentage[i] >= 80)
+                grade[i] = 'A';
+            else if (percentage[i] >= 70)
+                grade[i] = 'B';
+            else if (percentage[i] >= 60)
+                grade[i] = 'C';
+            else if (percentage[i] >= 50)
+                grade[i] = 'D';
+            else if (percentage[i] >= 40)
+                grade[i] = 'E';
+            else
+                grade[i] = 'R';
+        }
+
+        // Display result
+        System.out.println("\n--- Student Result ---");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Student " + (i + 1));
+            System.out.println("Physics: " + physics[i]);
+            System.out.println("Chemistry: " + chemistry[i]);
+            System.out.println("Maths: " + maths[i]);
+            System.out.println("Percentage: " + percentage[i] + "%");
+            System.out.println("Grade: " + grade[i]);
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
