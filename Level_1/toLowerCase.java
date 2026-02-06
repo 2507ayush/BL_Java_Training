19be1376506a648c79d529ffc1ae02f689fcf8e1
vermamaan55@gmail.com <vermamaan55@gmail.com>
2026-02-06 11:55:41 +0530

    Added toLowerCase.java, Solved by using String and it's function.

---
+++ Level_1/toLowerCase.java
@@ -0,0 +1,33 @@
+package Java.Level_1;
+import java.util.*;
+public class toLowerCase{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String c = Convert(s);
+        String c1 = s.toLowerCase();
+        System.out.println(c);
+        System.out.println(c1);
+        if(c.equals(c1)){
+            System.out.println("Both Are Equal");
+        }
+        else{
+            System.out.println("Both are Un'Equal");
+        }
+        sc.close();
+    }
+    public static String Convert(String s){
+        String s1="";
+        for(int i=0;i<s.length();i++){
+            char k = s.charAt(i);
+            if(k>=65 && k<=90){
+                char n = (char)(k+32);
+                s1 = s1 + n;
+            }
+            else{
+                s1=s1+k;
+            }
+        }
+        return s1;
+    }
+}
