f52a24fc70aaa1e16d8424786ee08524d23c2cb8
vermamaan55@gmail.com <vermamaan55@gmail.com>
2026-02-09 11:41:33 +0530

    Modified BinarySearch.java

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,30 +1,40 @@
 package Java.Searching;
+
 import java.util.*;
-public class BinarySearch{
-    public static void main(String args[]){
+
+public class BinarySearch {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
         System.out.println("Enter value to search");
         int val = sc.nextInt();
-        boolean res = Searching(arr,val);
-        if(res){
+        boolean res = Searching(arr, val);
+        if (res) {
             System.out.println("Value found");
-        }
-        else{
+        } else {
             System.out.println("Value not found");
         }
         sc.close();
     }
-    public static boolean Searching(int[] arr, int val){
+
+    public static boolean Searching(int[] arr, int val) {
         boolean flag = false;
         int l = arr.length;
-        for(int i=0;i<l;i++){
-            if(arr[i]==val){
+        Arrays.sort(arr);
+        int left = arr[0];
+        int right = arr[l - 1];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == val) {
                 flag = true;
+            } else if (arr[mid] < val) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return flag;
